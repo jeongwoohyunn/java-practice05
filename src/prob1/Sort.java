8dca03b4d5d2d1de82fc5937135e31d156c7de33
bit <bit@DESKTOP-74J9BGV>
2019-01-03 16:46:47 +0900

    prob2

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -1,22 +1,36 @@
 package prob1;
 
 public class Sort {
-	
+
 	public static void main(String[] arg) {
-	
+
 		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
 		int temp = 0;
-		int count =  array.length;
-		
-		System.out.println( "Before sort." );
-		
+		int count = array.length;
+
+		System.out.println("Before sort.");
+
 		for (int i = 0; i < count; i++) {
-			System.out.print( array[ i ] + " " );
+			System.out.print(array[i] + " ");
 		}
-		
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		// 
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i+1; j < array.length-1; j++) {
+				if (array[i] < array[j]) {
+					temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
+
+		//
+		System.out.println();
+		System.out.println("After Sort");
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}
 
 	}
 }
--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -1,4 +1,25 @@
 package prob2;
 
 public class SmartPhone extends MusicPhone {
+	@Override
+	public void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+	//이중상속 보충
+	private void call(){
+		System.out.println( "통화기능시작" );
+	}
+	@Override
+	public void execute( String function ) {
+		if ( function.equals("앱") ) {
+			playApp();
+			return;
+		}
+		
+		super.execute( function );
+	}
+	
+	protected void playApp() {
+		System.out.println("앱실행");
+	}
 }
